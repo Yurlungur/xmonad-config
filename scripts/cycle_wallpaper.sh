52730890d0daca6c3618402ec9177e1772d0f259
Jonah Miller <jonah.maxwell.miller@gmail.com>
2021-07-31 17:53:19 -0600

    cycle wallpaper

---
+++ scripts/cycle_wallpaper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# cycle_wallpaper.sh
+# Author: Yurlungur 
+# This script will cycle your wallpaper every time T
+# amount of time passes. T is given in command line and the syntax is:
+# 2 = 2 seconds, 1m = 1 minute, 2h = 2 hours
+# Call the script as a daemon. For instance:
+# cycle_wallpaper.sh 1h &
+
+# The script we call to rotate wallpapers
+ROTATOR=/home/$(whoami)/myscripts/randomwallpaper.py
+
+# Cycle the wallpaper once before waiting for the next rotation
+python3 $ROTATOR
+
+# The never ending while loop which cycles the wallpaper
+while true; do
+    sleep $@
+    python3 $ROTATOR
+done
